8aa8f0f31e8d2c5944c4f9a6b62e853e1d855fc2
AMAbdelbasir1 <addtooo140@gmail.com>
2023-09-09 19:04:00 +0300

    test request

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const request = require("request");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const paymentRoute  = require("./routes/paymentRouter");
+const paymentRoute = require("./routes/paymentRouter");
 app.use("/paymob", paymentRoute);
+setInterval(function () {
+  request.get(
+    "http://localhost:3000/paymob",
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+    (error, response) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response.body);
+      }
+    },
+  );
+}, 300000);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
